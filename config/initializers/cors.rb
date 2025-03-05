6ff3027c17e198060b51f70d1ec3d693f5b13d4d
Dev-Moulin <p.moulin.95@gmail.com>
2025-03-05 17:39:45 +0100

    add config devise and devise-jwt + add key for jwt in credentials + add file controller api (registration and sessions)

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,14 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'http://localhost:5173' # URL du frontend Vite par défaut
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      expose: ['Authorization'],
+      credentials: true
+  end
+end
